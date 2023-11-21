93b893dad57f4afcb196c814f0f389e518268669
Singgih Pratama <singgihpratama96@gmail.com>
2023-11-21 20:25:14 +0700

    initialize project

---
+++ src/test/java/tea/tech/teablog/TeablogApplicationTests.java
@@ -0,0 +1,13 @@
+package tea.tech.teablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeablogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
